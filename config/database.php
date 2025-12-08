2a184f8012b57eb29b9bf000fa0fc53cd38b6e11
Wel Wel Gallardo <welwelgallardo0724@gmail.com>
2025-12-08 16:36:05 +0800

    Fix TiDB SSL configuration

--- config/database.php
+++ config/database.php
@@ -44,8 +44,8 @@ return [
 'mysql' => [
     'driver' => 'mysql',
     'url' => env('DATABASE_URL'),
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
+    'host' => env('DB_HOST', 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com'),
+    'port' => env('DB_PORT', '4000'),
     'database' => env('DB_DATABASE', 'forge'),
     'username' => env('DB_USERNAME', 'forge'),
     'password' => env('DB_PASSWORD', ''),
